406383453ff9c1a2b93134b5dee916532520e056
Desarrollo <copoliyo@gmail.com>
2015-04-01 17:43:50 +0200

    Commit inicial desde Despacho

---
+++ src/ezkabamaniacos/CoordenadaGeografica.java
@@ -0,0 +1,34 @@
+package ezkabamaniacos;
+
+
+public class CoordenadaGeografica{
+	
+    private double longitud;
+    private double latitud;
+
+    public CoordenadaGeografica() {
+        this.longitud = 0.0;
+        this.latitud = 0.0;
+    }
+
+    public CoordenadaGeografica(double longitud, double latitud) {
+        this.longitud = longitud;
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+}
